05a65b4cedb32aa6635c7820176ba2c21b7dcac4
snehasamala <snehasamala438@gmail.com>
2026-02-15 15:34:26 +0530

    added files related to project

--- ansible/inventories/dynamic_inventory.sh
+++ ansible/inventories/dynamic_inventory.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-# Read IPs from Terraform output
-FRONTEND_IP=$(terraform -chdir=../terraform output -raw frontend_ip)
-BACKEND_IP=$(terraform -chdir=../terraform output -raw backend_ip)
 
-cat << EOF
-[frontend]
-c8.local ansible_host=$FRONTEND_IP ansible_user=ec2-user
+FRONTEND_IP=$(terraform -chdir=../terraform output -raw frontend_ip 2>/dev/null)
+BACKEND_IP=$(terraform -chdir=../terraform output -raw backend_ip 2>/dev/null)
 
-[backend]
-u21.local ansible_host=$BACKEND_IP ansible_user=ubuntu
+if [[ "$1" == "--list" ]]; then
+cat <<EOF
+{
+  "frontend": {
+    "hosts": ["c8.local"]
+  },
+  "backend": {
+    "hosts": ["u21.local"]
+  },
+  "_meta": {
+    "hostvars": {
+      "c8.local": {
+        "ansible_host": "$FRONTEND_IP",
+        "ansible_user": "ec2-user"
+      },
+      "u21.local": {
+        "ansible_host": "$BACKEND_IP",
+        "ansible_user": "ubuntu"
+      }
+    }
+  }
+}
 EOF
+else
+  echo "{}"
+fi
